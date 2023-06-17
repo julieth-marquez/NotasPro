a9912a59be1b9df47d7520eaadbf1c89cb25034b
Karla <APRENDIZ@BOGDAFCGFFSD173.FormacionSena.RED>
2023-06-16 21:18:46 -0500

    Estructura docente y administrador

--- Administrador/modelos/administrador.php
+++ Administrador/modelos/administrador.php
@@ -9,10 +9,10 @@ public function __construct()
 	$this->db = parent::__construct();
 
 	}
-
-	public function agregarad($Nombread, $Apellidoad, $Usuarioad, $Passwordad, $Perfilad, $Estadoad)
+//Inserta un usuario
+	public function agregarad($Nombread,$Apellidoad,$Usuarioad,$Passwordad, $Perfilad,$Estadoad)
 	{
-		$statement = $this->db->prepare("INSERT INTO usuarios(Nombreusu, Apellidousu, Usuario, Passwordusu, Perfil, Estado )values(:Nombread,:Apellidoad, :Usuarioad, :Passwordad, :Perfilad, :Estadoad )");
+		$statement = $this->db->prepare("INSERT INTO usuarios(Nombreusu, Apellidousu, Usuario, Passwordusu, Perfil, Estado )values(:Nombread,:Apellidoad, :Usuarioad, :Passwordad, :'Administrador', :'Activo' )");
 
 		$statement->bindParam(":Nombread",$Nombread);
 		$statement->bindParam(":Apellidoad",$Apellidoad);
@@ -24,20 +24,81 @@ public function __construct()
 		if($statement->execute())
 		{
 			echo"Usuario registrado";
+			header('Location: ../pages/index.php');
 
 		}else
 		{
 
 			echo"No se puede realizar el registro ";
+			header('Location: ../pages/agregar.php');
 		}
 	}
 
 
+//Función para seleccionar todos los usuarios con el rol administrador
+	public function getad()
+	{
+		$row = null; 
+		$statement=$this->db->prepare("SELECT * FROM usuarios WHERE Perfil='Administrador'");
+		$statement->execute();
+		while($resul = $statement->feth()) 
+		{
+			$row[]=$resul;
+		}
+		return $row;
 
 
+	}
+//Función para selecionar un usuario por su ID
+	public function getidad($Id)
+	{
+		$row = null;
+		$statement=$this->db->prepare("SELECT * FROM usuarios WHERE Perfil='Administrador'AND id_usuario=:Id");
+		$statement->bindParam(':Id',$Id);
+		$statement->execute();
+		while ($resul = $statement->feth()) 
+		{
+			$row=$resul;
+		}
+		return $row;
+	}
+//Función para seleccionar los datos del usuario 
+	public function updatead($Id,$Nombread,$Apellidoad,$Usuarioad,$Passwordad, $Perfilad,$Estadoad)
+	{
+		$statement=$this->db->prepare("UPDATE usuarios SET Nombreusu=:Nombread,Apellidousu=:Apellidoad,Usuario=:Usuarioad,Passwordusu=:Passwordad,Estado=:Estadoad WHERE id_usuario=$Id");
 
+		$statement->bindParam(':Id',$Id);
+		$statement->bindParam(':Nombread',$Nombread);
+		$statement->bindParam(':Apellidoad',$Apellidoad);
+		$statement->bindParam(':Usuarioad',$Usuarioad);
+		$statement->bindParam(':Passwordad',$Passwordad);
+		$statement->bindParam(':Estadoad',$Estadoad);
+		if($statement->execute())
+		{
+			header('Location: ../pages/index.php')
+		}else
+		{
+			header('Location:../pages/editar.php');
+		}
 
+	}
 
+//función para eliminar un usuario 
+	public function deletead($Id)
+	{
+		$statement=$this->db->prepare("DELETE * FROM usuarios WHERE id_usuario=:Id");
+		$statement->bindParam(':Id',$Id);
+		if ($statement->execute()) 
+		{
+			echo"Usuario eliminado";
+			header('Location:../pages/index.php');
+		}else
+		{
+			echo"El Usuario no se puede eliminar";
+			header('Location:../pages/eliminar.php');
+		}
+
+}
 
 
 }
--- Docentes/modelos/docente.php
+++ Docentes/modelos/docente.php
@@ -1,7 +1,98 @@
 <?php
+include_once('../../conexion.php');
+class Docentes extends Conexion
+ {
+ 	 public function __construct()
+ 	 {
+ 	 	$this->db = parent::__construct();
+ 	 }
+//Insertar Docente 
+ 	 public function creardocen($Nombredocen,$Apellidodocen,$Documentodocen,$Correodocen,$Materiadocen,$Notasdocen)
+ 	 {
+ 	 	$statement = $this->db->prepare("INSERT INTO docentes (Nombredoc, Apellidodoc, Documentodoc, Correodoc, Materiadoc, Notasmate )values(:Nombredocen,:Apellidodocen, :Documentodocen, :Correodocen, :Materiadocen, :Notasdocen )");
 
-class Docentes {
+		$statement->bindParam(":Nombredocen",$Nombredocen);
+		$statement->bindParam(":Apellidodocen",$Apellidodocen);
+		$statement->bindParam(":Documentodocen",$Documentodocen);
+		$statement->bindParam(":Correodocen",$Correodocen);
+		$statement->bindParam(":Materiadocen",$Materiadocen);
+		$statement->bindParam(":Notasdocen",$Notasdocen);
 
+		if($statement->execute())
+		{
+			echo"Docente registrado";
+			header('Location: ../pages/index.php');
+
+		}else
+		{
+
+			echo"No se puede realizar el registro del docente ";
+			header('Location: ../pages/agregar.php');
+		}
+ 	 }
+
+ //Función para seleccionar a todos los usuarios con el rol docentes 
+ 	 public function getdocen()
+	{
+		$row = null; 
+		$statement=$this->db->prepare("SELECT * FROM docentes");
+		$statement->execute();
+		while($resul = $statement->feth()) 
+		{
+			$row[]=$resul;
+		}
+		return $row;
+	}
+//Función para selecionar un docente por su ID 
+	public function getiddocen($Id)
+	{
+		$row = null;
+		$statement=$this->db->prepare("SELECT * FROM docentes WHERE id_docente= :$Id");
+		$statement->bindParam(':Id',$Id);
+		$statement->execute();
+		while ($resul = $statement->feth()) 
+		{
+			$row=$resul;
+		}
+		return $row;
+	}
+//Función para selecionar los datos del docente 
+	public function updatedocen($Id,$Nombredocen,$Apellidodocen,$Documentodocen,$Correodocen, $Materiadocen,$Notasdocen)
+	{
+		$statement=$this->db->prepare("UPDATE docentes SET Nombredoc=:Nombredocen,Apellidodoc=:Apellidodocen,Documentodoc=:Documentodocen,Correodoc=:Correodocen,Materiadoc=:Materiadocen,Notasmate=:Notasdocen WHERE id_docente=$Id");
+
+		$statement->bindParam(':Id',$Id);
+		$statement->bindParam(':Nombredocen',$Nombredocen);
+		$statement->bindParam(':Apellidodocen',$Apellidodocen);
+		$statement->bindParam(':Documentodocen',$Documentodocen);
+		$statement->bindParam(':Correodocen',$Correodocen);
+		$statement->bindParam(':Materiadocen',$Materiadocen);
+		$statement->bindParam(':Notasdocen',$Notasdocen);
+		if($statement->execute())
+		{
+			header('Location: ../pages/index.php')
+		}else
+		{
+			header('Location:../pages/editar.php');
+		}
+
+	}
+//Función para eliminar un docente 
+	public function deletedocen($Id)
+	{
+		$statement=$this->db->prepare("DELETE * FROM docentes WHERE id_docente=:Id");
+		$statement->bindParam(':Id',$Id);
+		if ($statement->execute()) 
+		{
+			echo"Docente eliminado";
+			header('Location:../pages/index.php');
+		}else
+		{
+			echo"El docente no se puede eliminar";
+			header('Location:../pages/eliminar.php');
+		}
+
+}
 
 	
 }
